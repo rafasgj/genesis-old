85d7a9517f299df0f8deda743a1e79d766c9678e
Rafael Guterres Jeffman <rafasgj@gmail.com>
2019-04-08 15:41:12 -0300

    Added a configuration class for the game.
    Issues: None

---
+++ config.py
@@ -0,0 +1,11 @@
+"""General game configuration"""
+
+
+class Config:  # noqa: D204
+    """A data object to hold game configuration."""
+    pass
+
+
+config = Config()
+config.player_speed = 4
+config.fps = 60
